bd4b0e0bbd79c1524aa7cf319237c54c6f9e1310
graig79 <graig79@gmail.com>
2014-11-03 12:03:17 -0500

    Setup files for use with Carbon

---
+++ carbon.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/lge/d851/full_d851.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := carbon_d851
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
